1f1f85c112b412273b35c1c64920e93419564366
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-07-14 17:31:06 -0700

    wrote tests in spec file

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,45 @@
-const enhancer = require('./enhancer.js');
-// test away!
+const enhancer = require("./enhancer.js");
+
+describe("enhancer unit tests", () => {
+  it("repairs", () => {
+    // expect(enhancer.repair(name, durability, enhancement)).toEqual({})
+    expect(
+      enhancer.repair({ name: "string", durability: 100, enhancement: 20 })
+    ).toEqual({ name: "string", durability: 100, enhancement: 20 });
+    expect(
+      enhancer.repair({ name: "string", durability: 50, enhancement: 10 })
+    ).toEqual({ name: "string", durability: 100, enhancement: 10 });
+    expect(
+      enhancer.repair({ name: "string", durability: 0, enhancement: 0 })
+    ).toEqual({ name: "string", durability: 100, enhancement: 0 });
+  });
+
+  it("succeeds", () => {
+    // expect(enhancer.repair(name, durability, enhancement)).toEqual({name: "string", durability: 100, enhancement: 20})
+    expect(
+      enhancer.succeed({ name: "string", durability: 100, enhancement: 20 })
+    ).toEqual({ name: "string", durability: 100, enhancement: 20 });
+    expect(
+      enhancer.succeed({ name: "string", durability: 51, enhancement: 10 })
+    ).toEqual({ name: "string", durability: 51, enhancement: 11 });
+    expect(
+      enhancer.succeed({ name: "string", durability: 0, enhancement: 0 })
+    ).toEqual({ name: "string", durability: 0, enhancement: 1 });
+  });
+
+  it("fails", () => {
+    // expect(enhancer.repair(name, durability, enhancement)).toEqual({})
+    expect(
+      enhancer.fail({ name: "string", durability: 100, enhancement: 19 })
+    ).toEqual({ name: "string", durability: 90, enhancement: 18 });
+    expect(
+      enhancer.fail({ name: "string", durability: 51, enhancement: 10 })
+    ).toEqual({ name: "string", durability: 46, enhancement: 10 });
+    expect(
+      enhancer.fail({ name: "string", durability: 0, enhancement: 0 })
+    ).toEqual({ name: "string", durability: 0, enhancement: 1 });
+    expect(
+      enhancer.fail({ name: "string", durability: 100, enhancement: 15 })
+    ).toEqual({ name: "string", durability: 90, enhancement: 15 });
+  });
+});
